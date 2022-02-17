e00d8ccd4a9f19a34a6a8a986b790a02fb0d399b
aisudev <tk.thanatorn@gmail.com>
2022-02-17 15:23:32 +0700

    update module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { withController } from './hoc/withController'
 import { GlobalProvider, useController } from './core/GlobalController'
-
+import { ThemeProvider } from '@mui/material'
+import { theme } from './core/theme'
 function App() {
   const global = useController()
   return (
-    <div>
-      <h1>{global.test}</h1>
-    </div>
+    <ThemeProvider
+      theme={theme}
+    >
+      <div>
+        <h1>{global.test}</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
