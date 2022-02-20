799a6fe6bc836191013f5991d23f92cb7d51f40b
nontanut-p <76061264+nontanut-p@users.noreply.github.com>
2022-02-20 13:06:41 +0700

    Add files via upload
    UploadText

--- src/components/common/UploadText.js
+++ src/components/common/UploadText.js
@@ -1,9 +1,56 @@
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Container, Grid, Typography ,Box, Paper,Button} from "@mui/material"
 
-export default function UploadText() {
 
-    return (
-        <>
-            <input type="file" id="file-selector" />
-        </>
-    )
+
+function UploadText() {
+  const [Content, setContent] = useState('');
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+      //console.log('contents',file)
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = function(e) {
+        var content = reader.result;
+      
+        setContent(content)
+        console.log(Content);
+    }
+    reader.readAsText(file);  
+    })
+
+
+
+    
+  })
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  
+  return (
+        <Box>
+        <Box sx={{ textAlign: 'left',p: 1,color:'#6390CB',  fontWeight: 500 , fontSize : 18, border: '1px dashed grey' , width:'50%'}}>  
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop your text file here, or click to select file</p>
+          <div>
+        </div>
+        </div>
+        </Box>
+        <Box sx={{ textAlign: 'left', m:5, lineHeight: 2 ,  fontWeight: 500 , fontSize : 16 ,color:'#6390CB', width:'50%' }}> 
+        <Grid container alignContent="flex-end">
+        <Typography variant="subtitle1" gutterBottom component="div">
+        {Content}
+        </Typography>
+        </Grid>
+         </Box>   
+         </Box>
+
+  )
 }
+  
+  
+
+ 
+
+export default UploadText;
