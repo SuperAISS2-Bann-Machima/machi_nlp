4d1ca02d59dd01a04d15da62d56a4d37fa7bdcd7
nontanut-p <76061264+nontanut-p@users.noreply.github.com>
2022-02-20 13:50:24 +0700

    Add files via upload
    Add Showbox and filename

--- src/components/common/UploadText.js
+++ src/components/common/UploadText.js
@@ -6,19 +6,27 @@ import { Container, Grid, Typography ,Box, Paper,Button} from "@mui/material"
 
 function UploadText() {
   const [Content, setContent] = useState('');
+  const [FileName, setFileName] = useState('')
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader()
-      //console.log('contents',file)
+      
+      
+      
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
       reader.onload = function(e) {
         var content = reader.result;
-      
+        var fileName = file.name
         setContent(content)
-        console.log(Content);
+        console.log(Content,fileName);
+        setFileName(fileName)
+
+        
     }
     reader.readAsText(file);  
+    
+    
     })
 
 
@@ -29,21 +37,25 @@ function UploadText() {
   
   return (
         <Box>
-        <Box sx={{ textAlign: 'left',p: 1,color:'#6390CB',  fontWeight: 500 , fontSize : 18, border: '1px dashed grey' , width:'50%'}}>  
+        <Box sx={{ textAlign: 'center',p: 1,color:'#6390CB',  fontWeight: 500 , fontSize : 18,  borderColor: 'grey.500' , width:'50%', borderRadius:'5%'}}>  
         <div {...getRootProps()}>
           <input {...getInputProps()} />
-          <p>Drag 'n' drop your text file here, or click to select file</p>
+          
+          {FileName ?<p>Drag 'n' drop again if you want to change text file</p>: <p>Drag 'n' drop your text file here, or click to select file</p>}
+         
           <div>
         </div>
         </div>
+        
         </Box>
-        <Box sx={{ textAlign: 'left', m:5, lineHeight: 2 ,  fontWeight: 500 , fontSize : 16 ,color:'#6390CB', width:'50%' }}> 
-        <Grid container alignContent="flex-end">
-        <Typography variant="subtitle1" gutterBottom component="div">
+        
+        {Content  ? <Box sx={{ textAlign: 'left', m:5, lineHeight: 2 , boxShadow: 3  , fontWeight: 500 , fontSize : 16 , width:'50%' , borderRadius:'5%' , bgcolor:'#E5E5E5'}}> 
+        <Typography sx={{  p: 2   }} variant="subtitle1" gutterBottom component="div">
+        <p>FileName : {FileName}</p>    
         {Content}
         </Typography>
-        </Grid>
-         </Box>   
+         </Box>  : null} 
+
          </Box>
 
   )
