34b91cb80cd36cfd0d5345786a6040ea458f80b2
aisudev <tk.thanatorn@gmail.com>
2022-02-22 10:43:30 +0700

    not found page

--- src/features/notfound/index.js
+++ src/features/notfound/index.js
@@ -1,5 +1,22 @@
+import { Typography } from "@mui/material";
+
 export default function NotFound() {
     return (
-        <h1>NotFound</h1>
+        <div style={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography
+                variant="h1"
+                style={{
+                    fontFamily: 'Skranji',
+                    background: "linear-gradient(45deg,#222668 50%, #6390CB 100%)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                }}
+                textAlign='center'
+            >
+                404
+                <br />
+                Not Found!
+            </Typography>
+        </div>
     )
 }
